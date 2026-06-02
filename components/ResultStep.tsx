581a8299609ea72c8e4bc01f3992973d9d541ac3
DavidMarioLC <davidmariolc.dev@gmail.com>
2026-06-02 11:54:51 -0500

    feat: add pdf link

--- components/ResultStep.tsx
+++ components/ResultStep.tsx
@@ -17,20 +17,45 @@ export default function ResultStep({ eligible }: ResultStepProps) {
       className="flex flex-col md:flex-row items-center gap-3 flex-wrap"
     >
       {eligible ? (
-        <>
-          <p className="text-white text-sm md:text-base">Felicidades, puedes ser</p>
-          <Image
-            src="/images/team-sangre-logo.png"
-            alt="Team Sangre"
-            width={100}
-            height={100}
-            style={{ height: "auto" }}
-          />
-        </>
+        <div className="flex flex-col items-center gap-5 text-white text-sm md:text-base">
+          <div className="flex gap-2 flex-col md:flex-row items-center">
+            <p>Felicidades, puedes ser</p>
+            <Image
+              src="/images/team-sangre-logo.png"
+              alt="Team Sangre"
+              width={100}
+              height={100}
+              style={{ height: "auto" }}
+            />
+          </div>
+          <p>
+            Descubre cómo{" "}
+            <a
+              target="_blank"
+              href="https://fmdshcczcfkshfhihvjf.supabase.co/storage/v1/object/public/team-sangre/team-sangre.pdf"
+              rel="noopener"
+              className="underline underline-offset-4"
+            >
+              aquí
+            </a>
+            .
+          </p>
+        </div>
       ) : (
-        <p className="text-white text-sm md:text-base">
-          Lo sentimos, no puedes ser Team Sangre
-        </p>
+        <div className="flex flex-col text-sm md:text-base  gap-5 text-center text-white ">
+          <p>¡Casi! Aún no, pero pronto podrías ser Team Sangre.</p>
+          <p>
+            Descubre cómo{" "}
+            <a
+              target="_blank"
+              href="https://fmdshcczcfkshfhihvjf.supabase.co/storage/v1/object/public/team-sangre/team-sangre.pdf"
+              rel="noopener"
+              className="underline underline-offset-4"
+            >
+              aquí.
+            </a>
+          </p>
+        </div>
       )}
     </motion.div>
   );
